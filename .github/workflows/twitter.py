8c6c9836c9862a8d51be69277d0192c4d1b7335d
James Gabriel <jamesozgabriel808@gmail.com>
2024-03-08 20:00:14 +0100

    Update twitter.py
    saved the secrets to github secrets

--- .github/workflows/twitter.py
+++ .github/workflows/twitter.py
@@ -1,12 +1,19 @@
+import os
 import tweepy
 import requests
 from bs4 import BeautifulSoup
 
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAABVZmQEAAAAAatqB%2FkOx%2Bp5mn5pv2mh324CqxBs%3DaqjmGOBVmM0CVKAs9IyjUAjEm3gNk5WAg77cx8iH7Os3kfiMO4'
+bearer_token = os.environ.get('BEARER_TOKEN')
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_secret = os.environ.get('ACCESS_SECRET')
+
+"""bearer_token = 'AAAAAAAAAAAAAAAAAAAAABVZmQEAAAAAatqB%2FkOx%2Bp5mn5pv2mh324CqxBs%3DaqjmGOBVmM0CVKAs9IyjUAjEm3gNk5WAg77cx8iH7Os3kfiMO4'
 consumer_key = 'pwyZBxzrCn2xkhWV6kYnN5orF'
 consumer_secret = 'IPZXeOXw3AmgJDZYVuFpIEh6Rzo3p0CLPgNUew555gVymhLuYt'
 access_token = '1528686812130066433-w66qdkFxA8Px2gmXluF5yVQWnSFQ8I'
-access_secret = 'ijv2RpvAFzr0a4GK9DmRaRyld6kK4m8Ez7UxFZVOnaUzF'
+access_secret = 'ijv2RpvAFzr0a4GK9DmRaRyld6kK4m8Ez7UxFZVOnaUzF'"""
 
 client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret,
                       access_token=access_token, access_token_secret=access_secret)
